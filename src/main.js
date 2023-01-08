b4c7e09d69006842be730f2e5a5f5b9eb1b36c90
XamzatJR <bahaev.hamzat95@gmail.com>
2023-01-08 17:03:00 +0300

    feat: add functions: addUsers, removeUsers, fetchUsers. Change user's content in addUser function

--- src/main.js
+++ src/main.js
@@ -1,13 +1,42 @@
 const usersList = document.querySelector('.users');
+const searchBar = document.querySelector('#search-bar');
+let timerID = null;
 document.addEventListener('DOMContentLoaded', async () => {
-  let users = await fetch('https://dummyjson.com/users')
-    .then((data) => data.json())
-    .then((users) => users.users);
-  users.map((el) => addUser(el));
+  let users = await fetchUsers();
+  addUsers(users);
 });
 function addUser(el) {
   let userItem = document.createElement('li');
-  userItem.textContent = `${el.firstName} ${el.lastName}`;
+  userItem.innerHTML = `
+  <img class="user__img" src="${el.image}" alt="">
+  <div class="user-content">
+    <div class="user__title">
+    ${el.firstName} ${el.lastName}  
+    </div>
+    <div class="user__subtitle">
+    ${el.address.city}, ${el.address.address.replace(/^[^\s]+/, '')}
+    </div>
+    <div class="user__subtitle">
+    ${el.university}
+    </div>  
+  </div>
+  `;
   userItem.id = el.id;
+  userItem.className = 'user';
   usersList.append(userItem);
+}
+
+function fetchUsers() {
+  return fetch('https://dummyjson.com/users')
+    .then((data) => data.json())
+    .then((users) => users.users);
+}
+function addUsers(users) {
+  users.forEach((el) => addUser(el));
+}
+function removeUsers(parent) {
+  let childNodes = parent.childNodes;
+  for (let child of childNodes) {
+    child.remove();
+  }
 }
